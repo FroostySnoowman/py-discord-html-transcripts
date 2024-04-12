2fa257fdb89137ddebf83a157ea0c3dc7b734356
FroostySnoowman <jacob.j.beal@icloud.com>
2024-04-11 22:56:34 -0600

    modified:   pyproject.toml 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py-discord-html-transcripts",
+    name="py_discord_html_transcripts",
     version="1.0.0",
     author="FroostySnoowman",
     author_email="froostysnoowmanbusiness@gmail.com",
